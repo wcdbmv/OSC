6e7e887edcf458deb880d38cb5d375e9c910f132
Kerimov Ahmed <kerimov.dev@yandex.ru>
2020-12-02 00:56:43 +0300

    Improve code

--- usb_boot_authentication.c
+++ usb_boot_authentication.c
@@ -14,9 +14,10 @@ MODULE_DESCRIPTION(UBA_MODULE_DESCRIPTION);
 MODULE_AUTHOR(UBA_MODULE_AUTHOR);
 MODULE_LICENSE(UBA_MODULE_LICENSE);
 
-static struct task_struct *agetty_stop_thread;
+static bool program_has_terminated;
+static struct task_struct *suspend_agetty_process;
 
-static bool set_task_state(const char *comm, long state)
+static bool uba_set_task_state(const char *comm, long state)
 {
 	struct task_struct *task;
 
@@ -30,25 +31,30 @@ static bool set_task_state(const char *comm, long state)
 	return false;
 }
 
-static int uba_agetty_stop_thread(void *data)
+static void uba_stop_thread(struct task_struct **thread)
+{
+	kthread_stop(*thread);
+	*thread = NULL;
+}
+
+static int uba_suspend_agetty_process(void *data)
 {
 	int i;
 
-	while (!kthread_should_stop() && !set_task_state("agetty", TASK_UNINTERRUPTIBLE)) {
+	while (!uba_set_task_state("agetty", TASK_UNINTERRUPTIBLE) && !kthread_should_stop()) {
 		ssleep(1);
 	}
 
-	for (i = 30; !kthread_should_stop() && i >= 0; --i) {
+	for (i = 30; i >= 0 && !kthread_should_stop(); --i) {
 		printk(KERN_NOTICE "%s: %02d secs to shutdown\n", UBA_MODULE_NAME, i);
-		printk(KERN_NOTICE "%s: plug in usb key to start up\n", UBA_MODULE_NAME);
+		printk(KERN_NOTICE "%s: plug in the USB key to start\n", UBA_MODULE_NAME);
 		ssleep(1);
 	}
 	if (i < 0) {
 		kernel_power_off();
 	}
 
-	printk(KERN_DEBUG "%s: uba_agetty_stop_thread stopped\n", UBA_MODULE_NAME);
-
+	printk(KERN_DEBUG "%s: uba_suspend_agetty_process stopped\n", UBA_MODULE_NAME);
 	return 0;
 }
 
@@ -61,14 +67,21 @@ static int uba_probe(struct usb_interface *interface, const struct usb_device_id
 	struct usb_device *dev;
 
 	dev = interface_to_usbdev(interface);
+	printk(KERN_NOTICE "%s: USB flash drive plugged\n", UBA_MODULE_NAME);
+	printk(KERN_NOTICE "%s: VID: 0x%04X, PID: 0x%04X, Serial: %s\n", UBA_MODULE_NAME, id->idVendor, id->idProduct, dev->serial);
 
-	printk(KERN_NOTICE "%s: USB flash drive (0x%04X:0x%04X) [Serial=%s] plugged\n", UBA_MODULE_NAME, id->idVendor, id->idProduct, dev->serial);
-	if (strcmp(dev->serial, UBA_SERIAL)) {
-		printk(KERN_NOTICE "%s: The usb serial does not match\n", UBA_MODULE_NAME);
+	if (!program_has_terminated) {
+		if (strcmp(dev->serial, UBA_SERIAL)) {
+			printk(KERN_NOTICE "%s: The serial of the USB flash drive doesn't match\n", UBA_MODULE_NAME);
+			return -1;
+		}
+
+		uba_stop_thread(&suspend_agetty_process);
+		uba_set_task_state("agetty", TASK_INTERRUPTIBLE);
+
+		printk(KERN_NOTICE "%s: USB boot authentication completed successfully\n", UBA_MODULE_NAME);
+		program_has_terminated = true;
 	}
-
-	kthread_stop(agetty_stop_thread);
-	set_task_state("agetty", TASK_INTERRUPTIBLE);
 
 	return 0;
 }
@@ -96,14 +109,16 @@ static int __init usb_boot_authentication_init(void)
 {
 	int rc;
 
-	if (IS_ERR(agetty_stop_thread = kthread_run(uba_agetty_stop_thread, NULL, "uba_agetty_stop_thread"))) {
-		rc = (int) PTR_ERR(agetty_stop_thread);
-		printk(KERN_ERR "%s: kthread_run failed with code %d\n", UBA_MODULE_NAME, rc);
-		return rc;
-	}
+	program_has_terminated = false;
 
 	if ((rc = usb_register(&uba_driver)) < 0) {
 		printk(KERN_ERR "%s: usb_register failed with code %d\n", UBA_MODULE_NAME, rc);
+		return rc;
+	}
+
+	if (IS_ERR(suspend_agetty_process = kthread_run(uba_suspend_agetty_process, NULL, "uba_suspend_agetty_process"))) {
+		rc = (int) PTR_ERR(suspend_agetty_process);
+		printk(KERN_ERR "%s: kthread_run failed with code %d\n", UBA_MODULE_NAME, rc);
 		return rc;
 	}
 
@@ -113,7 +128,12 @@ static int __init usb_boot_authentication_init(void)
 
 static void __exit usb_boot_authentication_exit(void)
 {
+	if (suspend_agetty_process) {
+		uba_stop_thread(&suspend_agetty_process);
+	}
+
 	usb_deregister(&uba_driver);
+
 	printk(KERN_DEBUG "%s: module unloaded\n", UBA_MODULE_NAME);
 }
 
